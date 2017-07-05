341b5b6b0864a1ea87e0a18c2edfcc6d26d90af5
Eitan Greene <email": null>
2017-07-05 16:05:04 -0700

    Delete counter

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -1,13 +1,8 @@
-var counter = 0
-
 function popUp () {
   event.preventDefault()
-
-  if (counter % 2 === 0) {
-    $('.about').fadeIn(500)
-  } else {
-    $('.about').fadeOut(500)
-  }
-
-  counter += 1
+  $('.popup').fadeIn(500)
+  $(".cover").fadeTo(500, 0.8)
 }
+
+// $('.video-container').fadeOut(500)
+// $(".cover").fadeOut(500)
