3a0511d186fb65569e0c760be03e1f168caa378f
Eitan Greene <email": null>
2017-07-05 17:45:45 -0700

    Reset video upon modal close

--- app/assets/javascripts/popup.js
+++ app/assets/javascripts/popup.js
@@ -1,8 +1,14 @@
+$(document).ready(function () {
+  $('.cover').click(function (event) {
+    $('.popup').fadeOut(300)
+    $('.cover').fadeOut(300)
+    var videoSource = $('#video').attr('src')
+    $('#video').attr('src', videoSource)
+  })
+})
+
 function popUp () {
   event.preventDefault()
   $('.popup').fadeIn(500)
-  $(".cover").fadeTo(500, 0.8)
+  $('.cover').fadeTo(500, 0.9)
 }
-
-// $('.video-container').fadeOut(500)
-// $(".cover").fadeOut(500)
